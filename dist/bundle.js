3a9e117be9cc81df33e8894bf570977b79ebf126
Alexys Hegmann <alexys.hegmann@gmail.com>
2016-12-09 23:08:20 +0000

    Added examples

--- dist/bundle.js
+++ dist/bundle.js
@@ -591,10 +591,11 @@ var WIDTH = 100,
     MODE_TEXT = 0x0;
     
 var CHARMAP = [
-  null,'+','-','*','/','_','.',',',';','>','<','?','!','"','(',')',
-  '0','1','2','3','4','5','6','7','8','9','=',':','[',']','{','}',
-  ' ','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
-  'P','Q','R','S','T','U','V','W','X','Y','Z','@','#','$','%','&'
+      //  0    1   2   3   4   5   6   7   8   9   A   B   C   D   E   F
+/* 0 */  null,'+','-','*','/','_','.',',',';','>','<','?','!','"','(',')',
+/* 1 */  '0' ,'1','2','3','4','5','6','7','8','9','=',':','[',']','{','}',
+/* 2 */  ' ' ,'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
+/* 3 */  'P' ,'Q','R','S','T','U','V','W','X','Y','Z','@','#','$','%','&'
 ];
 
 var memPerPageByMode = {
@@ -697,19 +698,30 @@ window.cpu = cpu;
 
 var program = new Uint8Array([
   
-  // Print chartmap in screen
-  42, 4, 0xE0,  // MOV XL, 0xE0
+  // Print "Hello, World!"
+  54, 0xF, // JMP 0xF ; To the begining of execution
+  
+  // Data
+  // H     E     L     L     O     ,    W     O     R     L     D     !    <null>
+     0x28, 0x25, 0x2C, 0x2C, 0x2F, 0x7, 0x37, 0x2F, 0x32, 0x2C, 0x24, 0xC, 0x0,
+     
+  // Start program
+  
+  42, 4, 0xE0,  // MOV XL, 0xE0 ; Set X to Screen mem
   42, 5, 0xF6,  // MOV XH, 0xF6
   
-  17, 0,        // INC A
-  39, 1, 0,     // MOV B, A
-  26, 1, 0xC0,  // OR B, 0xC0
-  43, 20, 1,    // MOV [X], B
-  17, 20,       // INC X
+  42, 0, 0x02,  // MOV A, 0x02  ; point string to A
   
-  76, 0, 63,    // CMP A, 63
-  60, 6,        // JNE 0x6
-  0             // HLT
+  40, 1, 0,     // MOV B, [A]   ; Add color flags
+  76, 1, 0,     // CMP B, 0     ; Compare char with null
+  57, 0x2C,     // JE 0x2C      ; Jump out of loop if null found
+  26, 1, 0xC0,  // OR B, 0xC0   ;  to char into B from A pointer
+  43, 20, 1,    // MOV [X], B   ; Print char to screen
+  17, 20,       // INC X        ; Increment screen pointer
+  17, 0,        // INC A        ; Increment string pointer
+  54, 0x18,     // JMP 0x18     ; Jump back to the loop
+  
+  0,            // HLT
 
 ]);
 
